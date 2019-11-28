cc08335c4615ffc65ac231e2f80c05f1f90eb5ea
Jesse Zhou <jesse.zhou@gusto.com>
2019-11-27 22:10:40 -0800

    fixed linter errors + imported packages

--- src/hooks/useGravatar.js
+++ src/hooks/useGravatar.js
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
-import md5 from "md5";
+import { useState, useEffect } from 'react';
+import md5 from 'md5';
 
-const EMAIL = "jesse.cy.zhou@gmail.com";
+const EMAIL = 'jesse.cy.zhou@gmail.com';
 const GRAVATAR_PROFILE_URL = `https://en.gravatar.com/${md5(EMAIL)}.json`;
 
 const useGravatar = () => {
   const [profile, setProfile] = useState({});
 
   useEffect(() => {
+    // eslint-disable-next-line no-undef
     fetch(GRAVATAR_PROFILE_URL)
-      .then(response => response.json())
-      .then(gravatarProfile => setProfile(gravatarProfile));
+      .then((response) => response.json())
+      .then((gravatarProfile) => setProfile(gravatarProfile));
   }, []);
 
   return profile;
